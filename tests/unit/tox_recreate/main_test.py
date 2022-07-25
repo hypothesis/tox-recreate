89cbac00f202a214fa5f66df070d4e2d457c8321
Sean Hammond <seanh@users.noreply.github.com>
2022-07-25 10:49:03 +0100

    wip

--- tests/unit/tox_recreate/main_test.py
+++ tests/unit/tox_recreate/main_test.py
@@ -1,2 +1,48 @@
-def test_it():
-    assert True
+from unittest.mock import MagicMock
+
+import pytest
+
+from tox_recreate.main import tox_configure
+
+
+class TestToxConfigure:
+    def test_with_no_cached_hash(self, config):
+        tox_configure(config)
+
+        assert not config.envconfigs["tests"].recreate
+        assert config.envconfigs["format"].recreate
+
+    def test_with_non_matching_cached_hash(self, config):
+        pass
+
+    def test_with_matching_cached_hash(self, config):
+        envconfig = config.envconfigs["format"]
+        with open(
+            Path(envconfig.envdir) / "tox_recreate.json", "w", encoding="utf8"
+        ) as file:
+            json.dump(cached_hashes, file)
+
+    @pytest.fixture
+    def config(self, envconfig):
+        return MagicMock(
+            spec_set=["envconfigs", "envlist"],
+            envconfigs={
+                "tests": envconfig("tests"),
+                "format": envconfig("format"),
+            },
+            envlist=["format"],
+        )
+
+
+@pytest.fixture
+def envconfig(tmp_path):
+    def envconfig(envname):
+        """Return a mock tox envconfig object."""
+        return MagicMock(
+            spec_set=["envdir", "envname", "recreate"],
+            envdir=tmp_path / envname,
+            envname=envname,
+            recreate=False,
+        )
+
+    return envconfig
